81b6286765435f48f00dd22efddef95dcc783495
niuliangtao <niuliangtao@qq.com>
2022-12-09 17:13:37 +0800

    add

--- noteworker/worker/base.py
+++ noteworker/worker/base.py
@@ -2,21 +2,20 @@ from notesecret import read_secret
 
 from .app import WorkApp
 
-global app
-app = None
+global _app
+_app = None
 
 
 def get_default_app():
     """
     get_default_app
     """
-    global app
-    if app is not None:
-        return app
-
-    app= WorkApp(
+    global _app
+    if _app is  None:
+        _app= WorkApp(
         "noteWorker",
         broker=read_secret(cate1="noteworker", cate2="workApp", cate3='broker') or "redis://localhost:6379/0",
         backend=read_secret(cate1="noteworker", cate2="workApp", cate3='backend') or "redis://localhost:6379/0"
     )
-    return app
+    return _app
+
